eddb4322a622a27f0e01948d526cc4d7b3610e8e
Jagjeet singh <130072503+SardarJagjeetSingh@users.noreply.github.com>
2024-02-17 16:50:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function smoothscroll() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('main'),
+        smooth: true
+    });
+    
+}
+
+function page4Animation() {
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var img = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${img})`
+        })
+    })
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 100,
+        freeMode: true,
+    });
+}
+
+function menuAnimation(){
+    var Menu = document.querySelector("nav h3")
+var full = document.querySelector(".full-scr")
+var navImg = document.querySelector("nav img")
+var flag = 0
+Menu.addEventListener('click', function () {
+    if (flag == 0) {
+        full.style.top = 0
+        navImg.style.opacity = 0
+        flag = 1
+    }
+    else {
+        full.style.top = "-100%"
+        navImg.style.opacity = 1
+        flag = 0
+    }
+})
+
+}
+
+function loader(){
+    var loader = document.querySelector("#loader")
+setTimeout(function(){
+   loader.style.top = "-100%"
+},4000)
+}
+
+smoothscroll();
+swiperAnimation();
+page4Animation();
+menuAnimation();
+loader();
